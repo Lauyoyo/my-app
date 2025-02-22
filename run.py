72e0c230e632834255682dfdedcf9e784e58eb57
Lauyoyo <1239403223@qq.com>
2025-02-22 22:32:59 +1000

    Create run.py

---
+++ run.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+
+def run_script(script_name):
+    print(f"=== Start running {script_name} ===")
+    result = subprocess.run(["python", f"src/{script_name}"], capture_output=True, text=Ture)
+    print(result.stdout)
+    if result.stderr:
+        print(f"Error:{result.stderr}")
+    print(f"=== End running {script_name} ===\n")
+
+if __name__ == "__main__":
+    print("==== Start running My APP ===")
+
+    run_script("hello_world.py")
+    run_script("calculator.py")
+    run_script("guess_number.py")
+    run_script("monitor_directory.py")
+
+    print("=== End running ===")
